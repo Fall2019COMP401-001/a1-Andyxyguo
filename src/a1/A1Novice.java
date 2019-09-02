7075efeea983c0f6bd90e97109c1a1122332da0f
Xiaoyi Guo <xyguo@live.unc.edu>
2019-09-02 02:42:22 -0400

    A

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -1,10 +1,12 @@
 package a1;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class A1Novice {
 
 	public static void main(String[] args) {
+		new DecimalFormat("0.00######");
 		
 		Scanner scan = new Scanner(System.in);
 		
