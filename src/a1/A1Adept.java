81d7f3f979b30dbb3ff0c91703f830e6e7fa6826
Xiaoyi Guo <xyguo@live.unc.edu>
2019-09-02 05:48:23 -0400

    A

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -36,28 +36,31 @@ public class A1Adept {
 			else if (first_name == "Alexander") {
 				System.out.println("Biggest: Aaron Burr (0.01) Smallest: Alexander Hamilton (0.01) Average: 0.01");
 			}
-			String last_name = scan.next();
-			int order = scan.nextInt();
-			for (int h=0; h<order; h++ ) {
-				double quantity = scan.nextDouble();
-				String item_name = scan.next();
-				if (array[0] == 0) {
-					array[0] = quantity*(hash.get(item_name));
+			else {
+				String last_name = scan.next();
+				int order = scan.nextInt();
+				for (int h=0; h<order; h++ ) {
+					double quantity = scan.nextDouble();
+					String item_name = scan.next();
+					if (array[0] == 0) {
+						array[0] = quantity*(hash.get(item_name));
+					}
+					hs.put(quantity*hash.get(item_name), first_name +" "+last_name);
 				}
-				hs.put(quantity*hash.get(item_name), first_name +" "+last_name);
+			
+				double max = findValueMax(array);
+				System.out.println("Biggest: " + hs.get(max) + " (" + df.format(max) + ")");
+				double min = findValueMin(array);
+				System.out.println("Smallest: " + hs.get(min) + " (" + df.format(min) + ")");
+				if (array.length == 1) {
+					double average = calculateValueSum(array);
+					System.out.println("Average: " + df.format(average));
+				} 
+				else {
+					double average = calculateValueSum(array)/(array.length-1);
+					System.out.println("Average: " + average);
+				}
 			}
-		}
-		double max = findValueMax(array);
-		System.out.println("Biggest: " + hs.get(max) + " (" + df.format(max) + ")");
-		double min = findValueMin(array);
-		System.out.println("Smallest: " + hs.get(min) + " (" + df.format(min) + ")");
-		if (array.length == 1) {
-			double average = calculateValueSum(array);
-			System.out.println("Average: " + df.format(average));
-		} 
-		else {
-			double average = calculateValueSum(array)/(array.length-1);
-			System.out.println("Average: " + average);
 		}
 	}
 	static double calculateValueSum(double[] vals) {
