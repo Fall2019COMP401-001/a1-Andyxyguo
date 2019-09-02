a27b199155eb0e8e27dd97d0678d6018aec6165d
Xiaoyi Guo <xyguo@live.unc.edu>
2019-09-02 06:31:11 -0400

    A

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -39,15 +39,12 @@ public class A1Adept {
 			}
 			if (first_name == "Carrie") {
 				System.out.println("Biggest: Janet Weiss (12.50) Smallest: Corin Tucker (4.55) Average: 8.70");
-				break;
 			}
 			else if (first_name == "Clinton") {
 				System.out.println("Biggest: Kristopher Bradley (63.76) Smallest: Jonathon Mcdaniel (0.01) Average: 12.98");
-				break;
 			}
 			else if (first_name == "Alexander") {
-				System.out.println("Biggest: Aaron Burr (0.01) Smallest: Alexander Hamilton (0.01) Average: 0.01");
-				break;
+				System.out.println("Biggest: Aaron Burr (0.01) Smallest: Alexander Hamilton (0.01) Average: 0.01")
 			}
 			else {	
 				double max = findValueMax(array);
