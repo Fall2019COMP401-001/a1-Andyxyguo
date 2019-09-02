be48679fcef98d247a4e8a35603cd6ee4af7415f
Xiaoyi Guo <xyguo@live.unc.edu>
2019-09-02 16:27:23 -0400

    A

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -1,6 +1,8 @@
 package a1;
 
 import java.util.Scanner;
+import java.text.DecimalFormat;
+import java.util.Hashtable;
 
 public class A1Jedi {
 
@@ -9,6 +11,26 @@ public class A1Jedi {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		int count = scan.nextInt();
+		DecimalFormat df = new DecimalFormat("0.00");
+		Hashtable<String, Integer> hash = new Hashtable<String, Integer>();
+		for (int i = 0; i < count; i++) {
+			String item_name = scan.next();
+			double price = scan.nextDouble();
+			hash.put(item_name, 0);
+		}
 		
+		int customers = scan.nextInt();
+		for (int a = 0; a< customers; a++) {
+			String first_name = scan.next();
+			String last_name = scan.next();
+			int order = scan.nextInt();
+			for (int h=0; h<order; h++ ) {
+				double quantity = scan.nextDouble();
+				String item_name = scan.next();
+				hash.put(item_name, hash.get(item_name)+1);
+			}
+		}
+		System.out.println(hash);
 	}
 }
