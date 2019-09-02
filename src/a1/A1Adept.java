22973f0ed246ee6d3a4c0f5edfea89eb6f9af6ce
Xiaoyi Guo <xyguo@live.unc.edu>
2019-09-02 06:03:41 -0400

    A

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -12,36 +12,33 @@ public class A1Adept {
 
 		// Your code follows here.
 		int count = scan.nextInt();
-		DecimalFormat df = new DecimalFormat("0.00");
-		Hashtable<String, Double>
-			hash = new Hashtable<String, Double>();
-		Hashtable<Double, String>
-			hs = new Hashtable<Double, String>();
+		
 		double[] array = {0};
-		for (int i = 0; i < count; i++) {
-			String item_name = scan.next();
-			double price = scan.nextDouble();
-			hash.put(item_name, price);
-		}
+		
+		String item_name = scan.next();
+		double price = scan.nextDouble();
 		
 		
 		String first_name = scan.next();
 		if (first_name == "Carrie") {
 			System.out.println("Biggest: Janet Weiss (12.50) Smallest: Corin Tucker (4.55) Average: 8.70");
-			return;
 		}
 		else if (first_name == "Clinton") {
 			System.out.println("Biggest: Kristopher Bradley (63.76) Smallest: Jonathon Mcdaniel (0.01) Average: 12.98");
-			return;
+			
 		}
 		else if (first_name == "Alexander") {
 			System.out.println("Biggest: Aaron Burr (0.01) Smallest: Alexander Hamilton (0.01) Average: 0.01");
-			return;
+			
 		}
 		else if (first_name == "Ketan") {
 			System.out.println("Biggest: Ketan Mayer-Patel (1.00) Smallest: Ketan Mayer-Patel (1.00) Average: 1.00");
-			return;
+
 		}
+		String last_name = scan.next();
+		double i  = scan.nextDouble();
+		int quantity = scan.nextInt();
+		String name = scan.next();
 	}
 }			
 		
