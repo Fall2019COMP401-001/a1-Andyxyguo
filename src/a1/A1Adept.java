b5646af86724d71ecbe144ee0f06877599816f14
Xiaoyi Guo <xyguo@live.unc.edu>
2019-09-02 04:59:19 -0400

    A

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -15,7 +15,7 @@ public class A1Adept {
 			hash = new Hashtable<String, Double>();
 		Hashtable<Double, String>
 			hs = new Hashtable<Double, String>();
-		double[] array = {};
+		double[] array = {0};
 		for (int i = 0; i < count; i++) {
 			String item_name = scan.next();
 			double price = scan.nextDouble();
@@ -30,8 +30,13 @@ public class A1Adept {
 			for (int h=0; h<order; h++ ) {
 				int quantity = scan.nextInt();
 				String item_name = scan.next();
-				for (int i = 0; i<array.length+1; i++) {
-					array[i] = quantity*hash.get(item_name);
+				if (array[0] == 0) {
+					array[0] = quantity*hash.get(item_name);
+				}
+				else {
+					for (int i = 0; i<array.length+1; i++) {
+						array[i] = quantity*hash.get(item_name);
+					}
 				}
 				hs.put(quantity*hash.get(item_name), first_name +last_name);
 			}
