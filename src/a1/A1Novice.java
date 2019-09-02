28fccba23c20772a66b4ef761cbc13e497bdbadf
Xiaoyi Guo <xyguo@live.unc.edu>
2019-09-02 02:33:36 -0400

    A

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -7,8 +7,23 @@ public class A1Novice {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-
+		
 		// Your code follows here.
+		int customers = scan.nextInt();
+		
+		for (int i=0; i<customers; i++) {
+			int total = 0;
+			String first_name = scan.next();
+			String last_name = scan.next();
+			int items = scan.nextInt();
+				for (int a=0; a<items; a++) {
+					int quantity = scan.nextInt();
+					String order_name = scan.next();
+					double price = scan.nextInt();
+					total += quantity*price;
+				}
+			System.out.println(first_name.charAt(0) + ". " + last_name + ": " + total );
+		}
 		
 	}
 }
