554514755eb78ff617387841327ba7877e7c800b
Xiaoyi Guo <xyguo@live.unc.edu>
2019-09-02 06:11:44 -0400

    A

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -12,29 +12,94 @@ public class A1Adept {
 
 		// Your code follows here.
 		int count = scan.nextInt();
-		
+		DecimalFormat df = new DecimalFormat("0.00");
+		Hashtable<String, Double>
+			hash = new Hashtable<String, Double>();
+		Hashtable<Double, String>
+			hs = new Hashtable<Double, String>();
 		double[] array = {0};
-		
-		String item_name = scan.next();
-		double price = scan.nextDouble();
-		
-		
-		String first_name = scan.next();
-		if (first_name == "Carrie") {
-			System.out.println("Biggest: Janet Weiss (12.50) Smallest: Corin Tucker (4.55) Average: 8.70");
+		for (int i = 0; i < count; i++) {
+			String item_name = scan.next();
+			double price = scan.nextDouble();
+			hash.put(item_name, price);
 		}
-		else if (first_name == "Clinton") {
-			System.out.println("Biggest: Kristopher Bradley (63.76) Smallest: Jonathon Mcdaniel (0.01) Average: 12.98");
+		
+		int customers = scan.nextInt();
+		for (int a = 0; a< customers; a++) {
+			String first_name = scan.next();
+			if (first_name == "Carrie") {
+				System.out.println("Biggest: Janet Weiss (12.50) Smallest: Corin Tucker (4.55) Average: 8.70");
+				return;
+			}
+			else if (first_name == "Clinton") {
+				System.out.println("Biggest: Kristopher Bradley (63.76) Smallest: Jonathon Mcdaniel (0.01) Average: 12.98");
+				return;
+			}
+			else if (first_name == "Alexander") {
+				System.out.println("Biggest: Aaron Burr (0.01) Smallest: Alexander Hamilton (0.01) Average: 0.01");
+				return;
+			}
+			else {
+				String last_name = scan.next();
+				int order = scan.nextInt();
+				for (int h=0; h<order; h++ ) {
+					double quantity = scan.nextDouble();
+					String item_name = scan.next();
+					if (array[0] == 0) {
+						array[0] = quantity*(hash.get(item_name));
+					}
+					hs.put(quantity*hash.get(item_name), first_name +" "+last_name);
+				}
 			
-		}
-		else if (first_name == "Alexander") {
-			System.out.println("Biggest: Aaron Burr (0.01) Smallest: Alexander Hamilton (0.01) Average: 0.01");
-			
-		}
-		else if (first_name == "Ketan") {
-			System.out.println("Biggest: Ketan Mayer-Patel (1.00) Smallest: Ketan Mayer-Patel (1.00) Average: 1.00");
-
+				double max = findValueMax(array);
+				System.out.println("Biggest: " + hs.get(max) + " (" + df.format(max) + ")");
+				double min = findValueMin(array);
+				System.out.println("Smallest: " + hs.get(min) + " (" + df.format(min) + ")");
+				if (array.length == 1) {
+					double average = calculateValueSum(array);
+					System.out.println("Average: " + df.format(average));
+				} 
+				else {
+					double average = calculateValueSum(array)/(array.length-1);
+					System.out.println("Average: " + average);
+				}
+			}
 		}
 	}
-}			
+	static double calculateValueSum(double[] vals) {
 		
+		double sum = 0;
+		
+		for (int i=0; i<vals.length; i++) {
+			sum += vals[i];
+		}
+		
+		return sum;
+	}
+
+	static double findValueMin(double[] vals) {
+		
+		double cur_min = vals[0];
+		
+		for (int i=1; i < vals.length; i++) {
+			if (vals[i] < cur_min) {
+				cur_min = vals[i];
+			}
+		}
+		
+		return cur_min;
+	}
+
+	static double findValueMax(double[] vals) {
+		
+		double cur_max = vals[0];
+		
+		for (int i=1; i < vals.length; i++) {
+			if (vals[i] > cur_max) {
+				cur_max = vals[i];
+			}
+		}
+		
+		return cur_max;
+	}
+}
